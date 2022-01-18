4aab3b42fa1c9ce8954cbcd8e181fc25ed0b4609
Edwin Cruz <edwin.cru13@hotmail.com>
2022-01-17 18:30:08 -0600

    just adding comments

--- App/Routes/NotificationRoutes.js
+++ App/Routes/NotificationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Router = express.Router();
 const { ListNotification, GetNotification } = require('../Controllers/NotificationController')
 
-//define routes from user
+//define routes using the controllers
 Router.get("/GetNotification", GetNotification);
 Router.get("/ListNotification", ListNotification);
 
